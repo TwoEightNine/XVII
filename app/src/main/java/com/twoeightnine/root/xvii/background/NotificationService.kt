6c43036eb29a3538e5fc97e7a1bd260d559325f9
Vladislav <butorovva@altarix.ru>
2018-01-29 21:09:39 +0400

    rhtfisrndjm;jnotnnfdjchvu

--- app/src/main/java/com/twoeightnine/root/xvii/background/NotificationService.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/background/NotificationService.kt
@@ -10,7 +10,6 @@ import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.os.Bundle
 import android.os.Handler
-import android.os.IBinder
 import android.os.Vibrator
 import android.support.v4.app.NotificationCompat
 import android.support.v4.content.LocalBroadcastManager
