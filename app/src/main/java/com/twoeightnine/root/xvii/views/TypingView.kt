07aa876a18766cd8afd30170c10821bf8b9b5e7d
twoeightnine <twoeightnine@list.ru>
2019-03-22 17:38:55 +0400

    new TypingView animation

--- app/src/main/java/com/twoeightnine/root/xvii/views/TypingView.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/views/TypingView.kt
@@ -1,22 +1,25 @@
 package com.twoeightnine.root.xvii.views
 
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.View
-import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import com.twoeightnine.root.xvii.R
+import com.twoeightnine.root.xvii.managers.Prefs
 import com.twoeightnine.root.xvii.managers.Style
 
 class TypingView : LinearLayout {
 
-    constructor(context: Context): super(context) {
+    constructor(context: Context) : super(context) {
         init()
     }
 
-    constructor(context: Context, attributes: AttributeSet): super(context, attributes) {
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes) {
         init()
     }
 
@@ -27,15 +30,40 @@ class TypingView : LinearLayout {
         val iv1 = view.findViewById<ImageView>(R.id.ivDot1)
         val iv2 = view.findViewById<ImageView>(R.id.ivDot2)
         val iv3 = view.findViewById<ImageView>(R.id.ivDot3)
-
         Style.forViewGroupColor(view.findViewById<RelativeLayout>(R.id.rlBack))
 
-        val scale = AnimationUtils.loadAnimation(context, R.anim.scale_loader)
-        val scale50 = AnimationUtils.loadAnimation(context, R.anim.scale_loader_50)
-
-        iv1.startAnimation(scale)
-        iv2.startAnimation(scale50)
-        iv3.startAnimation(scale)
+        ValueAnimator.ofFloat(0f, 1f).apply {
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.RESTART
+            duration = DURATION
+            addUpdateListener { animation ->
+                val value = animation.animatedValue as Float
+                iv1.setColor(getColor(value < EDGE_1))
+            }
+            addUpdateListener { animation ->
+                val value = animation.animatedValue as Float
+                iv2.setColor(getColor(value >= EDGE_1 && value < EDGE_2))
+            }
+            addUpdateListener { animation ->
+                val value = animation.animatedValue as Float
+                iv3.setColor(getColor(value >= EDGE_2))
+            }
+        }.start()
     }
 
+    private fun getColor(isWhite: Boolean) = when {
+        isWhite -> Color.WHITE
+        Prefs.isNight -> Color.LTGRAY
+        else -> Color.GRAY
+    }
+
+    private fun ImageView.setColor(color: Int) {
+        (drawable as? GradientDrawable)?.setColor(color)
+    }
+
+    companion object {
+        const val DURATION = 2000L
+        const val EDGE_1 = 0.27f
+        const val EDGE_2 = 0.73f
+    }
 }
