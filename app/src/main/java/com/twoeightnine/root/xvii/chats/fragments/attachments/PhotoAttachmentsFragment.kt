09e849c88570872af92913ba6e1277567a56e9c7
twoeightnine <twoeightnine@list.ru>
2019-03-23 18:43:07 +0400

    photo attachments fix

--- app/src/main/java/com/twoeightnine/root/xvii/chats/fragments/attachments/PhotoAttachmentsFragment.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/chats/fragments/attachments/PhotoAttachmentsFragment.kt
@@ -34,7 +34,10 @@ class PhotoAttachmentsFragment : BaseAttachmentsFragment<Photo>() {
 
     private fun onClick(photo: Photo) {
         val photos = ArrayList(adapter.items)
-        ImageViewerActivity.viewImages(context, photos, photos.indexOf(photo))
+        val position = photos.indexOf(photo)
+        val fromPos = if (position > 20) position - 20 else 0
+        val toPos = if (position < photos.size - 20) position + 20 else photos.size
+        ImageViewerActivity.viewImages(context, ArrayList(photos.subList(fromPos, toPos)), position - fromPos)
     }
 
     companion object {
