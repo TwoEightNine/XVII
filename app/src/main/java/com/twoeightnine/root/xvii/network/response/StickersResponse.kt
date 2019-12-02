eaa29ad3f69d33b21a9322e1873d8f033b8059de
twoeightnine <twoeightnine@list.ru>
2019-12-02 11:19:07 +0400

    add stickers that has no key words

--- app/src/main/java/com/twoeightnine/root/xvii/network/response/StickersResponse.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/network/response/StickersResponse.kt
@@ -2,6 +2,7 @@ package com.twoeightnine.root.xvii.network.response
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import com.twoeightnine.root.xvii.lg.Lg
 import com.twoeightnine.root.xvii.model.StickerMind
 import com.twoeightnine.root.xvii.model.attachments.Sticker
 import kotlinx.android.parcel.Parcelize
@@ -32,6 +33,28 @@ data class StickersResponse(
                 }
             }
         }
-        return ArrayList(stickers.sortedBy { it.stickerId }.distinctBy { it.stickerId })
+        val preparedStickers = ArrayList(stickers.sortedBy { it.stickerId }.distinctBy { it.stickerId })
+        val toAdd = arrayListOf<Int>()
+        for (i in preparedStickers.indices) {
+            if (i == 0) continue
+
+            val diff = preparedStickers[i].stickerId - preparedStickers[i - 1].stickerId
+            if (diff != 1 && diff <= ALLOWED_DIFF) {
+                val from = preparedStickers[i - 1].stickerId + 1
+                val to = preparedStickers[i].stickerId - 1
+                for (j in from..to) {
+                    toAdd.add(j)
+                }
+            }
+        }
+        for (stickerId in toAdd) {
+            preparedStickers.add(Sticker(stickerId))
+        }
+        return ArrayList(preparedStickers.sortedBy { it.stickerId }.distinctBy { it.stickerId })
+    }
+
+    companion object {
+
+        const val ALLOWED_DIFF = 7
     }
 }
