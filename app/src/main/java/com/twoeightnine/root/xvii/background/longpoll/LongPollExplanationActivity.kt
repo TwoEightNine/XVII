c4a13a4eaa1220dd2c5cf31956af69b10a0b55fe
twoeightnine <twoeightnine@list.ru>
2020-04-06 18:37:55 +0400

    create longpoll explanation

--- app/src/main/java/com/twoeightnine/root/xvii/background/longpoll/LongPollExplanationActivity.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/background/longpoll/LongPollExplanationActivity.kt
@@ -1,13 +1,21 @@
 package com.twoeightnine.root.xvii.background.longpoll
 
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.twoeightnine.root.xvii.R
 import com.twoeightnine.root.xvii.activities.BaseActivity
+import com.twoeightnine.root.xvii.utils.setBottomInsetPadding
+import com.twoeightnine.root.xvii.utils.setTopInsetPadding
+import kotlinx.android.synthetic.main.activity_explanation.*
 
 class LongPollExplanationActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explanation)
+        tvTitle.setTopInsetPadding()
+        svContent.setBottomInsetPadding()
     }
+
+    override fun getStatusBarColor() = ContextCompat.getColor(this, R.color.status_bar)
 }
