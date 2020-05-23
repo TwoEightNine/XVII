c0862df4a2fbc927d19d23d78273dcd7b911f35b
twoeightnine <twoeightnine@list.ru>
2020-05-23 13:35:45 +0400

    make video viewer fullscreen

--- app/src/main/java/com/twoeightnine/root/xvii/web/VideoViewerActivity.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/web/VideoViewerActivity.kt
@@ -4,7 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -28,19 +29,11 @@ class VideoViewerActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_video_viewer)
         val url = intent?.extras?.getString(URL) ?: return
-
-        window.decorView.systemUiVisibility =
-                // Tells the system that the window wishes the content to
-                // be laid out at the most extreme scenario. See the docs for
-                // more information on the specifics
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        // Tells the system that the window wishes the content to
-                        // be laid out as if the navigation bar was hidden
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-
-
 
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = object : WebViewClient() {
