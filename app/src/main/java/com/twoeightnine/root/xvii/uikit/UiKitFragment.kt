08a86c699321d7d55a8dee462c58954280120566
twoeightnine <twoeightnine@list.ru>
2020-09-27 15:33:08 +0400

    create color utils and new color manager for new ui kit

---
+++ app/src/main/java/com/twoeightnine/root/xvii/uikit/UiKitFragment.kt
@@ -0,0 +1,70 @@
+package com.twoeightnine.root.xvii.uikit
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.twoeightnine.root.xvii.R
+import com.twoeightnine.root.xvii.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_ui_kit.*
+
+class UiKitFragment : BaseFragment() {
+
+    override fun getLayoutId(): Int = R.layout.fragment_ui_kit
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addText("range color")
+        addColor(Munch.prevRangeColor)
+
+        addText("similar color")
+        addColor(Munch.prevSimilarColor)
+
+        addText("near color")
+        addColor(Munch.prevNearColor)
+
+        addText("main color")
+        addColor(Munch.color)
+
+        addText("near color")
+        addColor(Munch.nextNearColor)
+
+        addText("similar color")
+        addColor(Munch.nextSimilarColor)
+
+        addText("range color")
+        addColor(Munch.nextRangeColor)
+
+        addText("extra color")
+        addColor(Munch.extraColor)
+    }
+
+    private fun addColor(colorScope: Munch.ColorScope) {
+        val container = LinearLayout(context)
+        container.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 150)
+        container.orientation = LinearLayout.HORIZONTAL
+        for (color in colorScope.toList()) {
+            val view = View(context)
+            view.layoutParams = LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    1f
+            )
+            view.setBackgroundColor(color)
+            container.addView(view)
+        }
+        llColors.addView(container)
+    }
+
+    private fun addText(text: String) {
+        val view = TextView(context)
+        view.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        view.text = text
+        llColors.addView(view)
+    }
+}
