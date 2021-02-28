b29f692869c983a7eaf99b1671393f1c56547c26
twoeightnine <twoeightnine@list.ru>
2021-02-28 22:09:02 +0400

    improve loader

--- app/src/main/java/com/twoeightnine/root/xvii/uikit/XviiLoader.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/uikit/XviiLoader.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.view_loader.view.*
 class XviiLoader(context: Context, attributeSet: AttributeSet) : FrameLayout(context, attributeSet) {
 
     private var alwaysWhite = false
+    private var size = Size.USUAL
 
     init {
         initAttributes(attributeSet)
@@ -21,12 +22,33 @@ class XviiLoader(context: Context, attributeSet: AttributeSet) : FrameLayout(con
             } else {
                 setIndicatorColor(*Munch.nextAnalogy.map { it.color }.toIntArray())
             }
+            indicatorSize = size.indicatorSize
+            trackThickness = size.trackThickness
+            trackCornerRadius = size.trackThickness / 2
         }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val size = this.size.indicatorSize + EXTRA_PADDING
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY)
+        )
     }
 
     private fun initAttributes(attributeSet: AttributeSet) {
         val attrs = context.theme.obtainStyledAttributes(attributeSet, R.styleable.XviiLoader, 0, 0)
         alwaysWhite = attrs.getBoolean(R.styleable.XviiLoader_alwaysWhite, false)
+        size = Size.values()[attrs.getInt(R.styleable.XviiLoader_size, 0)]
         attrs.recycle()
+    }
+
+    companion object {
+        private const val EXTRA_PADDING = 14
+    }
+
+    enum class Size(val indicatorSize: Int, val trackThickness: Int) {
+        USUAL(80, 8),
+        SMALL(48, 6)
     }
 }
