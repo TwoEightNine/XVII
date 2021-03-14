0e2dda2b2ca15207e9462febfe5057c1c4eaea2c
twoeightnine <twoeightnine@list.ru>
2021-03-14 19:50:42 +0400

    add unsafe client for safe prime host, refactor retrofit

--- data/src/main/java/global/msnthrp/xvii/data/network/Retrofit.kt
+++ data/src/main/java/global/msnthrp/xvii/data/network/Retrofit.kt
@@ -4,39 +4,25 @@ import com.google.gson.ExclusionStrategy
 import com.google.gson.FieldAttributes
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import global.msnthrp.xvii.data.BuildConfig
 import global.msnthrp.xvii.data.crypto.safeprime.storage.retrofit.SafePrimeApiService
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 object Retrofit {
 
-    private const val TIMEOUT = 30L
-
     val safePrimeApiService by lazy {
-        createApiService(SafePrimeApiService::class.java)
+        createApiService(Host.SAFE_PRIME, SafePrimeApiService::class.java)
     }
 
-    private val defaultLoggingInterceptor = createLoggingInterceptor()
-    private val defaultGson = createGson()
-    private val defaultOkHttpClient = createOkHttpClient()
 
-    private fun createOkHttpClient(): OkHttpClient =
-            OkHttpClient.Builder()
-                    .addInterceptor(defaultLoggingInterceptor)
-                    .readTimeout(TIMEOUT, TimeUnit.SECONDS)
-                    .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
-                    .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
-                    .build()
-
-    private fun createLoggingInterceptor() = HttpLoggingInterceptor().apply {
-        level = when {
-            BuildConfig.DEBUG -> HttpLoggingInterceptor.Level.BODY
-            else -> HttpLoggingInterceptor.Level.NONE
-        }
+    private val defaultGson by lazy {
+        createGson()
+    }
+    private val defaultOkHttpClient by lazy {
+        OkHttp.createOkHttpClient()
+    }
+    private val unsafeOkHttpClient by lazy {
+        OkHttp.createUnsafeOkHttpClient()
     }
 
     private fun createGson(): Gson = GsonBuilder()
@@ -45,14 +31,19 @@ object Retrofit {
                 override fun shouldSkipField(f: FieldAttributes) = false
             }).create()
 
-    private fun createRetrofit() =
+    private fun createRetrofit(host: Host) =
             Retrofit.Builder()
                     .addConverterFactory(GsonConverterFactory.create(defaultGson))
-                    .baseUrl("https://2ton.com.au/")
-                    .client(defaultOkHttpClient)
+                    .baseUrl(host.baseUrl)
+                    .client(host.clientType.getMatchingClient())
                     .build()
 
-    private fun <T> createApiService(apiServiceClass: Class<T>): T =
-            createRetrofit().create(apiServiceClass)
+    private fun <T> createApiService(host: Host, apiServiceClass: Class<T>): T =
+            createRetrofit(host).create(apiServiceClass)
+
+    private fun ClientType.getMatchingClient() = when (this) {
+        ClientType.UNSAFE -> unsafeOkHttpClient
+        ClientType.DEFAULT -> defaultOkHttpClient
+    }
 
 }
