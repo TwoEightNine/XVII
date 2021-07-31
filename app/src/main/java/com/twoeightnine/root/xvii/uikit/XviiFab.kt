a680a34ba938b1d1ed98eeecc777e6983dcb06e7
twoeightnine <twoeightnine@list.ru>
2021-07-31 13:11:21 +0400

    make fab icon color adaptive

--- app/src/main/java/com/twoeightnine/root/xvii/uikit/XviiFab.kt
+++ app/src/main/java/com/twoeightnine/root/xvii/uikit/XviiFab.kt
@@ -20,13 +20,22 @@ package com.twoeightnine.root.xvii.uikit
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.graphics.Color
 import android.util.AttributeSet
+import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.twoeightnine.root.xvii.R
+import com.twoeightnine.root.xvii.managers.Prefs
 
 class XviiFab(context: Context, attributeSet: AttributeSet) : FloatingActionButton(context, attributeSet) {
 
     init {
         backgroundTintList = ColorStateList.valueOf(Munch.color.color)
+        val iconColor = when {
+            Prefs.colorBetterWithWhite -> Color.WHITE
+            else -> ContextCompat.getColor(context, R.color.main_text_light)
+        }
+        drawable.paint(iconColor)
     }
 
 }
